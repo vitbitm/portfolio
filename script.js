d52ed2d94c9a9437009ee1073f9ba4dc3691a127
vitbitm <vitbitm@gmail.com>
2025-08-14 09:51:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const helloBtn = document.getElementById("helloBtn");
+    
+    helloBtn.addEventListener("click", () => {
+      alert("Привет! Рад тебя видеть на моём сайте-визитке 👋");
+    });
+  });
+  
