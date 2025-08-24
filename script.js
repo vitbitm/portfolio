64fbd9afcdda2b2653611f7d4e83c08ed1f32c8a
vitbitm <vitbitm@gmail.com>
2025-08-24 19:28:00 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const helloBtn = document.getElementById("helloBtn");
-    
-    helloBtn.addEventListener("click", () => {
-      alert("Привет! Рад тебя видеть на моём сайте-визитке 👋");
-    });
-  });
-  
